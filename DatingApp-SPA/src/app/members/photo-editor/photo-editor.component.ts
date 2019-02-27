1c42dfbba5bee42ae35675aad032684e30fe949a
Tom Knoll <tom.knoll@allphi.eu>
2019-02-27 15:00:50 +0100

    Lint error

--- DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -39,7 +39,7 @@ export class PhotoEditorComponent implements OnInit {
       allowedFileType: ['image'],
       removeAfterUpload: true,
       autoUpload: false,
-      maxFileSize: 10 *1024 * 1024
+      maxFileSize: 10 * 1024 * 1024
     });
 
     this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
